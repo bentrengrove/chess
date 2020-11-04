47b58fcd16052f1f216da1dcf5db252161a9af8c
Ben Trengrove <ben@pocketcasts.com>
2020-11-05 08:45:26 +1000

    Better AI

--- app/src/main/java/com/bentrengrove/chess/AI.kt
+++ app/src/main/java/com/bentrengrove/chess/AI.kt
@@ -2,14 +2,24 @@ package com.bentrengrove.chess
 
 class AI(val color: PieceColor) {
     // This AI doesn't work at all currently
-    fun calculateNextMove(game: Game): Move? {
-        val moves = game.allMovesFor(color)
-                .map { it to game.doMove(it.from, it.to).valueFor(color.other()) } // Minimise the opponents value
+    fun calculateNextMove(game: Game, player: PieceColor): Move? {
+        return search(game, 0, player).getOrNull(0)?.first
+    }
+
+    fun search(game: Game, depth: Int, player: PieceColor): List<Pair<Move, Int>> {
+        return game.allMovesFor(game.turn)
+                .mapNotNull {
+                    val newGame = game.doMove(it.from, it.to)
+                    if (depth > 0 && game.turn == player) {
+                        val best = search(newGame, depth - 1, player).firstOrNull()?.second ?: return@mapNotNull null
+                        it to best
+                    } else {
+                        it to (newGame.valueFor(player) - newGame.valueFor(player.other()))
+                    }
+                }
+                .sortedByDescending { it.second }
                 .shuffled()
                 .distinctBy { it.second }
-                .sortedBy { it.second }
                 .toList()
-
-        return moves.getOrNull(0)?.first
     }
 }
